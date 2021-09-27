73c7ac6f922bf692a1c54445fdf7911b175eec0f
Muhammad Zourdy <zhafarmuhammad12@gmail.com>
2021-09-27 12:58:43 +0700

    :tada: initialize the project and configuring the boilerplate
    checked

---
+++ src/GlobalStyles.js
@@ -0,0 +1,33 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+
+      :root{
+          --purple-primary: #554DDE;
+          --accent-pink: #F44E77;
+          --neutral-light: #F2F6FF;
+          --lavender-secondary: #6A6D9E; /*Primary Font Color*/
+          --dark-primary: #16194F;
+          --border-colour: #CAD6F1;
+      }
+      *{
+          margin: 0;
+          padding: 0;
+          list-style: none;
+          box-sizing: border-box;
+          font-family: 'Nunito', sans-serif;
+          text-decoration: none;
+      }
+      
+      html,
+      body{
+          background-color: var(--neutral-light);
+          color: white;
+          font-size: 1.2rem;
+          scroll-behavior: smooth;
+      }
+      a{
+          color: inherit;
+      }
+
+`;
