1a42851caf60692b6430f6081b4c80113e2a27c9
Muhammad Zourdy <zhafarmuhammad12@gmail.com>
2021-09-27 21:09:16 +0700

    :lipstick: building the accordion section
    done

---
+++ src/components/AccordionSection/index.jsx
@@ -0,0 +1,53 @@
+import React from "react";
+
+import lines from "../../images/lines.svg";
+import questions from "../../questions";
+import { Question } from "../Question";
+import { InnerWrapper } from "../../styles/Wrapper";
+import styled from "styled-components";
+
+export const AccordionSection = () => {
+  return (
+    <Container>
+      <InnerWrapper>
+        <h3 className="small-heading">
+          Frequently <span>asked questions</span>
+        </h3>
+        <p className="c-para">
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Assumenda
+          maxime ipsa nam expedita dolorem distinctio illo ad doloribus atque
+          fuga, Nihil laboriosam beatae fugit.
+        </p>
+        <div className="lines">
+          <img src={lines} alt="" />
+        </div>
+
+        <div className="questions-con">
+          {questions.map((item) => {
+            return <Question key={item.id} {...item} />;
+          })}
+        </div>
+      </InnerWrapper>
+    </Container>
+  );
+};
+
+const Container = styled.section`
+  .c-para {
+    width: 60%;
+    margin: 0 auto;
+  }
+  .questions-con {
+    padding-top: 4rem;
+  }
+  .lines {
+    position: absolute;
+    left: 0;
+    top: 300%;
+    width: 100%;
+    z-index: -1;
+    img {
+      width: 100%;
+    }
+  }
+`;
---
+++ src/components/Question/index.jsx
@@ -0,0 +1,52 @@
+import React, { useState } from "react";
+import styled from "styled-components/macro";
+
+import plus from "../../images/plus.svg";
+import minus from "../../images/minus.svg";
+
+export const Question = ({ title, description }) => {
+  const [toggle, setToggle] = useState(false);
+
+  return (
+    <Container>
+      <div className="q-con">
+        <div className="toggle-title">
+          <h4>{title}</h4>
+          <button onClick={() => setToggle((toggle) => !toggle)}>
+            {toggle ? <img src={minus} alt="" /> : <img src={plus} alt="" />}
+          </button>
+        </div>
+        {toggle && <p>{description}</p>}
+      </div>
+    </Container>
+  );
+};
+
+const Container = styled.div`
+  background-color: #fff;
+  margin: 1rem 0;
+  padding: 1.4rem 1rem;
+  border-radius: 24px;
+  transition: all 0.4s ease-in-out;
+  box-shadow: 0px 25px 50px rgba(22, 25, 79, 0.05);
+  p {
+    transition: all 0.4s ease-in-out;
+  }
+  h4 {
+    color: #16194f;
+    transition: all 0.4s ease-in-out;
+    font-size: 1.3rem;
+  }
+  .toggle-title {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    transition: all 0.4s ease-in-out;
+    button {
+      background: transparent;
+      outline: none;
+      cursor: pointer;
+      border: none;
+    }
+  }
+`;
